55e41e5c4dd856a2e544dfacdc8dcead33333f8c
Ben Carson <quod27@gmail.com>
2016-09-20 11:27:00 -0500

    Initial commit

---
+++ src/test/java/edu/jsu/mcis/ConverterTest.java
@@ -0,0 +1,38 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.util.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ConverterTest {
+    private String csvString;
+    private String jsonString;
+
+    @Before
+    public void setUp() {
+
+    }
+    
+    @Test
+    public void testConvertCSVtoJSON() {
+        // You should test using the files in src/test/resources.
+        assertTrue(false);
+    }
+
+    @Test
+    public void testConvertJSONtoCSV() {
+        // You should test using the files in src/test/resources.
+        assertTrue(false);
+    }
+}
+
+
+
+
+
+
+
